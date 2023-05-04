4e440c1ca8530218a99a6366e286c5697335e1bb
Adam <adamssmith2003@gmail.com>
2023-05-04 22:52:36 +0400

    mySQL

--- pysql.py
+++ pysql.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host='localhost',
-  user="root",
-  password="root",
-  database = "sakila"
+  host='###',
+  user="###",
+  password="###",
+  database = "###"
 )
 
 mycursor = mydb.cursor()
